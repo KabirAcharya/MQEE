058ece35f286e40b3697269ee33a9a6e2a7a42c7
Kabir Acharya <62584316+KabirAcharya@users.noreply.github.com>
2021-05-11 20:41:23 +1000

    Experimental Apply Link Fix to All Links

--- Chrome/injection/js/linkfix.js
+++ Chrome/injection/js/linkfix.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.block_alp a[onclick]').forEach((el) => {
+document.querySelectorAll('a[onclick]').forEach((el) => {
   el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1"); 
   el.setAttribute('onclick', '');
   el.setAttribute('target', '_blank');
