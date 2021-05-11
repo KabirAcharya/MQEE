427890ae50efb10d31120ce23a9b315a106ab59a
Kabir Acharya <62584316+KabirAcharya@users.noreply.github.com>
2021-05-11 21:03:22 +1000

    New Linkfix Patch

--- Gecko/injection/js/linkfix.js
+++ Gecko/injection/js/linkfix.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('a[onclick]').forEach((el) => {
-  el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1");
   if (el.getAttribute("onclick") != "") {
+    el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1");
     el.setAttribute('target', '_blank');
   }
   el.removeAttribute('onclick');
