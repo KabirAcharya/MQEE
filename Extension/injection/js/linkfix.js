e8e5f3129294354c1def3677106277bf9006d48d
Kabir Acharya <62584316+KabirAcharya@users.noreply.github.com>
2021-05-12 18:54:42 +1000

    Improved Linkfix Regex

--- Extension/injection/js/linkfix.js
+++ Extension/injection/js/linkfix.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('a[onclick]').forEach((el) => {
   if (el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1") != el.getAttribute("onclick")) {
-    el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1");
+    el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\).*/g, "$1");
     el.setAttribute('target', '_blank');
   }
   el.removeAttribute('onclick');
