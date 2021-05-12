fe16a71eb199b08934f52ccbdb48289dae4aba02
Kabir Acharya <62584316+KabirAcharya@users.noreply.github.com>
2021-05-12 18:47:04 +1000

    Fixed issue with linkfix in niche case

--- Extension/injection/js/linkfix.js
+++ Extension/injection/js/linkfix.js
@@ -1,5 +1,5 @@
 document.querySelectorAll('a[onclick]').forEach((el) => {
-  if (el.getAttribute("onclick") != "") {
+  if (el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1") != el.getAttribute("onclick")) {
     el.href = el.getAttribute("onclick").replace(/.*\('(http.*)'\)/g, "$1");
     el.setAttribute('target', '_blank');
   }
